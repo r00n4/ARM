65b53dfc680dcbe0b5e7a394ef4f88fcae150de3
sht4nigga <riccomister@mail.ru>
2020-03-11 16:47:02 +0300

    try to launch
    again

---
+++ USB_MSD/Sample 2.1/_Project/Objects/system_mdr32f9qx.d
@@ -0,0 +1,6 @@
+.\objects\system_mdr32f9qx.o: RTE\Device\MDR1986BE92\system_MDR32F9Qx.c
+.\objects\system_mdr32f9qx.o: C:\git\ARM\Files\MDR32Fx.h
+.\objects\system_mdr32f9qx.o: C:\git\ARM\Files\core_cm3.h
+.\objects\system_mdr32f9qx.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\system_mdr32f9qx.o: C:\git\ARM\Files\system_MDR32F9Qx.h
+.\objects\system_mdr32f9qx.o: RTE\Device\MDR1986BE92\MDR32F9Qx_config.h
--- USB_MSD/norm/files/main.h
+++ USB_MSD/norm/files/main.h
@@ -1,13 +1,11 @@
-
-
-
+/*
 #ifndef MAIN_H													// (if NOT defined) constant existance check
 #define MAIN_H													// Renew the constant
-
 #endif																	// Suspended compilation ending
 
+#include "main.h"
+*/
 #include "MDR32Fx.h"                    // Device header
-#include "C:\git\ARM\USB_MSD\norm\files\main.h"
 
 int main (void) {
 
@@ -18,15 +16,15 @@ int main (void) {
 		MDR_RST_CLK->PER_CLOCK |= (1 << RST_CLK_PCLK_PORTC_Pos);
 		
 		/*Configuration of PC0 & PC1 lines*/	
-		MDR_PORTC->OE				|= 	((1 << 0) 	| (1 << 1)); 		// Data move direction 
+		MDR_PORTC->OE			|= 	((1 << 0) 	| (1 << 1)); 		// Data move direction 
 		MDR_PORTC->PULL 		&= ~((1 << 0) 	| (1 << 1)); 		// Connecting to the GND is OFF
 		MDR_PORTC->PULL 		&= ~((1 << 16)	| (1 << 17));		// Connecting to the Vcc is OFF
-		MDR_PORTC->ANALOG 	|= 	((1 << 0) 	| (1 << 1));		// Line mode is DIGITAL
+		MDR_PORTC->ANALOG 		|= 	((1 << 0) 	| (1 << 1));		// Line mode is DIGITAL
 		MDR_PORTC->FUNC 		&= ~((3 << 0) 	| (3 << 2));		// Input/Output
 		MDR_PORTC->PD 			&= ~((1 << 0) 	| (1 << 1));		// Managing line by Drivers
 		MDR_PORTC->PD 			&= ~((1 << 16) 	| (1 << 17));   // The Schmitt trigger is OFF
 		MDR_PORTC->PWR 			&= ~((3 << 0) 	| (3 << 2));		// PWR Register bits are Reseted
-		MDR_PORTC->PWR 			|= ((1 << 0) 		| (1 << 2));		// Edge angle
+		MDR_PORTC->PWR 			|= ((1 << 0) 	| (1 << 2));		// Edge angle
 		MDR_PORTC->GFEN 		&= ~((1 << 0) 	| (1 << 1));		// Digital filters are OFF
 		
 	}
